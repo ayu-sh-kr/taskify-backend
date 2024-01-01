0ca68f4678b0bec9694d1e298e1012ff15e22bf3
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-01-01 11:51:57 +0530

    default user for accessing and monitoring application

--- src/main/java/com/resotechs/Taskify.java
+++ src/main/java/com/resotechs/Taskify.java
@@ -1,13 +1,35 @@
 package com.resotechs;
 
+import com.resotechs.entities.User;
+import com.resotechs.enums.RoleType;
+import com.resotechs.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class Taskify {
+@RequiredArgsConstructor
+public class Taskify implements CommandLineRunner {
+
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public static void main(String[] args) {
         SpringApplication.run(Taskify.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        User user = new User();
+        user.setName("Ayush Jaiswal");
+        user.setEmail("ayush@gmail.com");
+        user.setPhone("8931007054");
+        user.setPassword(passwordEncoder.encode("password"));
+        user.setRoleType(RoleType.ADMIN);
+        user.setActive(true);
+        userRepository.save(user);
+    }
 }
